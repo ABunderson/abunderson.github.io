54c9ac46e7c78aa61a295f2296adbb683e631c36
ABunderson <bun18015@byui.edu>
2021-02-17 15:28:47 -0700

    faster lazy

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,21 +1,21 @@
 // get all imgs with data-src attribute
-const imagesToLoad = document.querySelectorAll("img[data-src]");
+const imagesToLoad = document.querySelectorAll('img[data-src]');
 
 // optional parameters being set for the IntersectionalObserver
 const imgOptions = {
   threshold: 1,
-  rootMargin: "0px 0px -50px 0px",
+  rootMargin: "0px 0px -25px 0px",
 };
 
 const loadImages = (image) => {
-  image.setAttribute("src", image.getAttribute("data-src"));
+  image.setAttribute('src', image.getAttribute('data-src'));
   image.onload = () => {
-    image.removeAttribute("data-src");
+    image.removeAttribute('data-src');
   };
 };
 
 // first check to see if Intersection Observer is supported
-if ("IntersectionObserver" in window) {
+if ('IntersectionObserver' in window) {
   const observer = new IntersectionObserver((items, observer) => {
     items.forEach((item) => {
       if (item.isIntersecting) {
