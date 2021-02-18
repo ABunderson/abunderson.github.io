28083d42456bed379a51672598a01cf0e1df3946
ABunderson <bun18015@byui.edu>
2021-02-17 20:59:25 -0700

    try something new

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -2,10 +2,10 @@
 const imagesToLoad = document.querySelectorAll('img[data-src]');
 
 // optional parameters being set for the IntersectionalObserver
-const imgOptions = {
+/*const imgOptions = {
   threshold: 1,
   rootMargin: '0px 0px 50px 0px'
-};
+};*/
 
 const loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
